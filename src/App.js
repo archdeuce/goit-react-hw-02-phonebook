143bb132b2cd1bac6db38ffb1b7a036a26156c77
Oleg Reshetilo <archdeuce@gmail.com>
2021-01-26 00:37:08 +0200

    reset filter on added contact

--- src/App.js
+++ src/App.js
@@ -24,6 +24,7 @@ class App extends Component {
     this.setState(prevState => {
       return {
         contacts: [newContact, ...prevState.contacts],
+        filter: '',
       };
     });
   };
